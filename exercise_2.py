9ad7dde4d40391f10edd2edc2099a382dcae3969
Binal Patel <binalp11@gmail.com>
2015-01-26 03:50:57 +0000

    Started Exercise 2. Replaced A and G with t and c respectively

---
+++ exercise_2.py
@@ -0,0 +1,10 @@
+from __future__ import division
+
+#this is exercise 2 - complementing DNA
+
+sequence = "ACTGATCGATTACGTATAGTATTTGCTATCATACATATATATCGATGCGTTCAT"
+
+sequence2 = sequence.replace("A", "t")
+
+seqeunce3 = sequence2.replace("C", "g")
+
