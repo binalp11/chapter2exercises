39acea748d2143d3bf556909d3981933bfe4a9d6
Binal Patel <binalp11@gmail.com>
2015-01-26 04:20:24 +0000

    Started exercise 4. Did part 1, extracted only exons

---
+++ exercise_4.py
@@ -0,0 +1,16 @@
+from __future__ import division
+
+#this is exercise 4 <- Splicing out introns
+
+#part 1
+
+sequence = "ATCGATCGATCGATCGACTGACTAGTCATAGCTATGCATGTAGCTACTCGATCGATCGATCGATCGATCGATCGATCGATCGATCATGCTATCATCGATCGATATCGATGCATCGACTACTAT"
+
+exon_1 = sequence[0:64]
+exon_2 = sequence[91:len(sequence)]
+
+print("Coding region: " + exon_1 + exon_2)
+
+
+
+
