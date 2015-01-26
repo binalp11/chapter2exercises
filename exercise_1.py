7c4b23825df2251056fb2dc2b6113ddc4ce2999f
Binal Patel <binalp11@gmail.com>
2015-01-26 03:31:29 +0000

    Started the Exercise and found A and T total

---
+++ exercise_1.py
@@ -0,0 +1,10 @@
+#This is exercise 1 -> Calculating AT content
+
+sequence = "ACTGATCGATTACGTATAGTATTTGCTATCATACATATATATCGATGCGTTCAT"
+
+length = len(sequence)
+
+A = sequence.count("A")
+
+T = sequence.count("T")
+
